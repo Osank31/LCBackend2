c8e8bceb0ae48c32cce7086a265e81efa1ccc84a
Osank31 <osankverma2004@gmail.com>
2026-05-15 13:27:53 +0000

    auth init

---
+++ 3004_authentication/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import "dotenv/config"
+import { APP_PORT } from "./constants/constants"
+import { appErrorHandler, genericErrorHandler } from "./middleware/errorHandler"
+import logger from "./config/logger.config"
+import { dbConnect } from "./config/db.config"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+
+
+app.use(appErrorHandler);
+app.use(genericErrorHandler);
+
+dbConnect()
+
+
+app.listen(APP_PORT || 3000, ()=>{
+    logger.info(`Server running on port ${APP_PORT || 3000}`)
+})
