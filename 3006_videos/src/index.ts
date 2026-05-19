e961cf1a14ea3b1d1941240cb2e6a540d096454a
Osank31 <osankverma2004@gmail.com>
2026-05-19 10:57:28 +0000

    video service init

---
+++ 3006_videos/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import "dotenv/config"
+import { APP_PORT } from "./constants/constants"
+import { appErrorHandler, genericErrorHandler } from "./middleware/errorHandler"
+import logger from "./config/logger.config"
+import { dbConnect } from "./config/db.config"
+
+const app = express()
+
+app.use(express.json())
+
+
+
+
+app.use(appErrorHandler);
+app.use(genericErrorHandler);
+
+dbConnect()
+
+
+app.listen(APP_PORT || 3000, ()=>{
+    logger.info(`Server running on port ${APP_PORT || 3000}`)
+})
